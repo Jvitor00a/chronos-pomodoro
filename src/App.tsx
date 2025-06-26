da4a9076638ef8103458379f3040adcf165fbb85
Jota <joaovitoralvesvieira@gmail.com>
2025-06-26 15:15:14 -0300

    Organizando components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { Heading } from "./components/Heading";
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
-  console.log("Oi");
-
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, a sunt
-        aliquid eius esse facere vitae laudantium similique praesentium alias,
-        ab minus totam soluta iste magni, at voluptatibus. Harum, hic?
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
