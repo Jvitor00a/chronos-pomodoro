b0037aaaabc6a13299d9323123bc8ccd619595ac
João Vitor Alves V. <160862296+Jvitor00a@users.noreply.github.com>
2025-07-07 16:31:52 -0300

    Criando templates e paginas do chronos

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,7 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon } from "lucide-react";
-import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
-import { useState } from "react";
+import { Home } from "./pages/Home";
 
 export function App() {
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    setNumero((prevState) => prevState + 1);
-  }
-
-  return (
-    <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              labelText="Task"
-              id="meuInput"
-              type="text"
-              placeholder="Digite algo"
-            />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem5</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color="green" />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from "lucide-react";
+import { DefaultButton } from "../DefaultButton";
+import { Cycles } from "../Cycles";
+import { DefaultInput } from "../DefaultInput";
+
+export function MainForm() {
+    return (
+                <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Task"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem5</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
+          </div>
+        </form>
+    )
+}
