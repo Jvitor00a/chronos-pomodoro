6222521785a4b7e5b0c271f53f6692e3cad491c0
João Vitor Alves V. <160862296+Jvitor00a@users.noreply.github.com>
2025-07-08 15:31:18 -0300

    Criando Models de Tasks

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import { TaskModel } from "./TaskModel";
+
+export type TaskStateModel = {
+  tasks: TaskModel[];
+  secondsRemaing: number;
+  formatedSecondsRemaning: string;
+  activedTask: TaskModel | null;
+  currentCycle: number;
+  config: {
+    workTime: number;
+    shortBreakTime: number;
+    longBreakTime: number;
+  };
+};
